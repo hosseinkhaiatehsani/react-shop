9a648d04f30f90c814f96a8e02fb011abb606d3e
Ho3ein <hos.kh93@gmail.com>
2022-01-07 13:16:24 +0330

    header component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shop-page/shop-page.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+import './header.styles.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <Link to="/" className="logo-container">
+                <Logo className="logo"/>
+            </Link>
+            <nav className="nav-menu">
+                <Link to="/shop" className="nav-item">
+                    SHOP
+                </Link>
+                <Link to="/contact" className="nav-item">
+                    CONTACT
+                </Link>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
