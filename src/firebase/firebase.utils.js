eb9aa1c95857c22ed380caf9967add600f9281d5
Ho3ein <hos.kh93@gmail.com>
2022-01-09 14:15:47 +0330

    integration firebase to application

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,7 +5,6 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import 'firebase/firestore';
-// import 'firebase/auth';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -28,22 +27,4 @@ const analytics = getAnalytics(app);
 export const provider = new GoogleAuthProvider();
 export const auth = getAuth();
 
-// export const signInWithGoogle = signInWithPopup(auth, provider);
-  // .then((result) => {
-  //   // This gives you a Google Access Token. You can use it to access the Google API.
-  //   const credential = GoogleAuthProvider.credentialFromResult(result);
-  //   const token = credential.accessToken;
-  //   // The signed-in user info.
-  //   const user = result.user;
-  //   // ...
-  // }).catch((error) => {
-  //   // Handle Errors here.
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   const email = error.email;
-  //   // The AuthCredential type that was used.
-  //   const credential = GoogleAuthProvider.credentialFromError(error);
-  //   // ...
-  // });
 
