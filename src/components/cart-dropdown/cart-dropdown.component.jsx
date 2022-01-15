d2a577169a44f34be84db63bb3227f817245977f
Ho3ein <hos.kh93@gmail.com>
2022-01-15 16:49:06 +0330

    empty cart proper messages, checkout button redirect to checkout page ,hide dropdown in checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { useNavigate } from "react-router-dom";
+import { toggleCartHidden } from '../../redux/cart/cart.action';
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -7,23 +10,34 @@ import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, dispatch }) => {
+    const navigate = useNavigate();
     return(
     <div className="cart-dropdown">
         <div className="cart-items">
             {
+                cartItems.length ?
                 cartItems.map( cartItem => 
                     <CartItem key={cartItem.id} item={cartItem} />
                 )
+                : (<span className="empty-cart">
+                    Cart is empty
+                </span>)
             }
         </div>
-        <CustomButton className='go-to-checkout custom-button'>GO TO CHECKOUT</CustomButton>
+        <CustomButton className='go-to-checkout custom-button' 
+        onClick={() => {
+            navigate('checkout');
+            dispatch(toggleCartHidden())
+        }}>
+            GO TO CHECKOUT
+        </CustomButton>
     </div>
     );
 }
 
-const mapStateToProps = state => ({
-    cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 })
 
 export default connect(mapStateToProps)(CartDropdown);
