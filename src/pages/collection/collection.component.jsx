1a8151bb38b2c8fa0cb1f676a8ca71c29dc31801
Ho3ein <hos.kh93@gmail.com>
2022-01-16 17:46:11 +0330

    render a component for each colleciton item

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,15 +3,22 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectCollection } from "../../redux/shop/shop.selector";
 
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
 import './collection.styles.scss';
 
 const CollectionPage = () => {
     const params = useParams();
     const collection = useSelector( (state) => selectCollection(params.collectionId)(state) )
-    console.log(collection)
+    const { title, items } = collection;
     return(
         <div className="collection-page">
-            COLLECTION PAGE
+            <h2 className="collection-page-title">{title}</h2>
+            <div className="collection-container">
+                {
+                    items.map( item => <CollectionItem key={item.id} item={item} /> )
+                }
+            </div>
         </div>
     )
 }
