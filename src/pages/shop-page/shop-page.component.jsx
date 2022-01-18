09a9bb43e40f12907b168ef89b8cdefa9e0ddd6e
Ho3ein <hos.kh93@gmail.com>
2022-01-18 16:54:27 +0330

    convert functional to class component, retrieve collections from firebase

--- src/pages/shop-page/shop-page.component.jsx
+++ src/pages/shop-page/shop-page.component.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import { converCollectionSnapshotToMap } from "../../firebase/firebase.utils";
 
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 import CollectionPage from "../collection/collection.component";
 
 import './shop-pages.styles.scss';
 
-const ShopPage = () => {
-    return(
-        <div className="shop-page">
-            <Routes>
-                <Route path='' element={<CollectionOverview />} />
-                <Route path={`:collectionId`} element={<CollectionPage />} />
-            </Routes>
-        </div>
-    )
+class ShopPage extends React.Component {
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount = () => {
+        const db = getFirestore();
+        const collectionRef = collection(db, 'collections');
+
+        this.unsubscribeFromSnapshot = onSnapshot(collectionRef, (snapshot) => {
+            converCollectionSnapshotToMap(snapshot)
+        })
+
+
+        // another method to get data just need to make function async
+        // and import getDocs
+        
+        // const querySnapshot = await getDocs(collectionRef);
+        // converCollectionSnapshotToMap(querySnapshot)
+
+    }
+
+    render() {
+        return(
+            <div className="shop-page">
+                <Routes>
+                    <Route path='' element={<CollectionOverview />} />
+                    <Route path={`:collectionId`} element={<CollectionPage />} />
+                </Routes>
+            </div>
+        );
+    }
 }
 
 export default ShopPage;
