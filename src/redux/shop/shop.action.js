74203df88dfba02432b48e16803b654c54bd2fc9
Ho3ein <hos.kh93@gmail.com>
2022-01-24 15:21:53 +0330

    actions created for all types and invoke them in a function(fetchCollectionsAsyncStart) that use thunk

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,6 +1,35 @@
 import ShopActionTypes from "./shop.types";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { converCollectionSnapshotToMap } from "../../firebase/firebase.utils";
 
-export const UpdateCollections = collectionsMap => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
+
+export const fetchCollectionsSuccess = collectionMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionMap
+});
+
+export const fetchCollectionsError = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_ERROR,
+    payload: errorMessage
+})
+
+export const fetchCollectionsAsyncStart = () => {
+    return async(dispatch) => {
+        try {
+            const db = getFirestore();
+            const collectionRef = collection(db, 'collections');
+            dispatch(fetchCollectionsStart());
+
+            const querySnapshot = await getDocs(collectionRef);
+            const collectionMap = converCollectionSnapshotToMap(querySnapshot);
+            dispatch(fetchCollectionsSuccess(collectionMap));
+        } catch (error) {
+            dispatch(fetchCollectionsError(error))
+        }
+    }
+
+}
+
