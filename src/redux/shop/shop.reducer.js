5a08926450957cee5eed5eead9027cfe0e7132fc
Ho3ein <hos.kh93@gmail.com>
2022-01-24 15:16:38 +0330

    modified with new types and new state values

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,15 +1,29 @@
 import ShopActionTypes from "./shop.types";
 
 const INISTIAL_SHOP_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
 }
 
 const ShopReducer = (state = INISTIAL_SHOP_STATE, action) => {
     switch (action.type) {
-        case ShopActionTypes.UPDATE_COLLECTIONS:
+        case ShopActionTypes.FETCH_COLLECTIONS_START:
+            return{
+                ...state,
+                isFetching: true
+            }
+        case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
             return {
                 ...state,
+                isFetching: false,
                 collections: action.payload
+            }
+        case ShopActionTypes.FETCH_COLLECTIONS_ERROR:
+            return {
+                ...state,
+                isFetching: false,
+                errorMessage: action.payload,
             }
         default:
             return state;
