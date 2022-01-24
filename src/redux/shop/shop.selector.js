16172b7c7f559599d86c58ee3e572b3f47c944b4
Ho3ein <hos.kh93@gmail.com>
2022-01-24 10:59:51 +0330

    selectCollection and selectShopCollectionPreview modified to response properly to get firebase shop data

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -10,10 +10,10 @@ export const selectShopCollection = createSelector(
 export const selectCollection = collectionUrlParam => 
     createSelector(
         [selectShopCollection],
-        collections => collections[collectionUrlParam]
+        collections => (collections ? collections[collectionUrlParam] : null)
     );
 
 export const selectShopCollectionPreview = createSelector(
     [selectShopCollection],
-    collections => Object.keys(collections).map( key => collections[key] )
+    collections => collections ? Object.keys(collections).map( key => collections[key] ) : []
 )
